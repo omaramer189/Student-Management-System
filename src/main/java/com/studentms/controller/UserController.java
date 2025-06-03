106b9696c6dd70539f71fedb2dc4f6b00295bbe9
omaramer189 <153132052+omaramer189@users.noreply.github.com>
2025-06-03 17:41:48 -0500

    Create UserController.java

---
+++ src/main/java/com/studentms/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.studentms.controller;
+
+import com.studentms.model.User;
+import com.studentms.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+}
