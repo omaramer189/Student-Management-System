b4f0b0c3810c1e2d99a8b6baab54d47c9ac5090d
omaramer189 <153132052+omaramer189@users.noreply.github.com>
2025-06-03 17:40:30 -0500

    Create UserService.java

---
+++ src/main/java/com/studentms/service/UserService.java
@@ -0,0 +1,24 @@
+package com.studentms.service;
+
+import com.studentms.model.User;
+import com.studentms.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
